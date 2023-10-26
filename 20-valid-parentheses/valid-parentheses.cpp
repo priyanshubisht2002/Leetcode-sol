9b14c326a734d0dd02b818cd8c93369a8738413a
Priyanshu <120446275+priyanshubisht2002@users.noreply.github.com>
2023-10-26 07:59:19 +0530

    Time: 0 ms (100.00%) | Memory: 6.7 MB (41.11%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,21 +1,50 @@
+// class Solution {
+// public:
+//     bool isValid(string s) {
+//         stack<char> st;
+//         for(int i=0;i<s.length();i++){
+//             if(st.empty()){
+//                 st.push(s[i]);
+//             }
+//             else if((st.top()== '(' && s[i]==')') || (st.top()== '[' && s[i]==']') || (st.top== '{' && s[i]=='}')){
+//                 st.pop();
+//             }
+//         else{
+//             st.push(s[i]);
+//         }
+//         }
+//         if(st.empty()){
+//             return true;
+//         }
+//         return false;
+        
+//     }
+// };
+
+
+
+// Method -2
 class Solution {
 public:
+unordered_map<char,int> symbols = {{'[',-1},{'{',-2},{'(',-3},{']',1},{'}',2},{')',3}};
     bool isValid(string s) {
         stack<char> st;
-        for(int i=0;i<s.length();i++){
-            if(st.empty()){
-                st.push(s[i]);
+        for(char brackets: s){
+            if(symbols[brackets]<0){
+                st.push(brackets);
             }
-            else if((st.top()== '(' && s[i]==')') || (st.top()== '[' && s[i]==']') || (st.top()== '{' && s[i]=='}')){
+            else{
+                if(st.empty()) return false;
+                char top = st.top();
                 st.pop();
+                if(symbols[top] + symbols[brackets]){
+                    return false;
+                }
+
             }
-        else{
-            st.push(s[i]);
+
         }
-        }
-        if(st.empty()){
-            return true;
-        }
+        if(st.empty()) return true;
         return false;
         
     }
